e2af0cd6759bacf896275b5f135e5db872c5ff18
kimi <hykimigoyal@gmail.com>
2026-06-26 12:31:06 +0530

    category drawer fixed

--- frontend/src/components/common/PublicRoute.tsx
+++ frontend/src/components/common/PublicRoute.tsx
@@ -1,31 +1,4 @@
-// import { Navigate, Outlet } from 'react-router-dom'
 
-// const PublicRoute = () => {
-//   const token = localStorage.getItem('token')
-
-//   if (token) {
-//     return <Navigate to="/home" replace />
-//   }
-
-//   return <Outlet />
-// }
-
-// export default PublicRoute
-// components/common/PublicRoute.tsx
-//  import { Navigate, Outlet } from 'react-router-dom'
-// import { useAuthStore } from "../../store/authStore"
-
-// const PublicRoute = () => {
-//   const { isAuthenticated, isGuest } = useAuthStore()
-
-//   if (isAuthenticated || isGuest) {
-//     return <Navigate to="/home" replace />
-//   }
-
-//   return <Outlet />
-// }
-
-// export default PublicRoute
 
 import { Navigate } from "react-router-dom";
 import { useAuthStore } from "../../store/authStore";
